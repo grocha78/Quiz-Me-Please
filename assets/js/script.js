332017feb4f46d40217bfbabd3730a97761f903e
Guadalupe Rocha <guadaluperocha@GuadalupesMBP2.attlocal.net>
2022-06-16 19:36:42 -0500

    created files

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+questions = Which company developed javascript?
+            Microsoft, Netscape, Apple, Adobe, Disney
+
+            Which of these is a looping structure in Javascript?
+            this, else, if, FOR, why
+
+            What would be the result of 3+2+"7"?
+            12, 42, 14, 28, Fiftyseven
+
+            Why is a Pop up box type available in javascript?
+            ALERT, Hey you, Psst, Ding-ding, Prompter
+
+            Does Javascript have concept level scope?
+            No  
